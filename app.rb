663c63cd7946cb7f2ffd70bb6eae4a4011650084
Romebomben <hugo.romeborn@elev.ga.ntig.se>
2025-10-03 14:25:40 +0200

    Nya forms

--- app.rb
+++ app.rb
@@ -1,13 +1,50 @@
 require 'sinatra'
 require 'slim'
 require 'sinatra/reloader'
+require 'sqlite3'
 
+
+post('/fruit') do
+  name = params[:name]
+  amount = params[:amount].to_i
+  db = SQLite3::Database.new("db/fruits.db")
+
+  datafrukt = db.execute("SELECT * FROM fruits WHERE name=?", namn)
+  datafrukt.each do |frukt|
+    frukt
+
+  end
+
+  db.execute("INSERT INTO fruits (name, amount) VALUES (?,?)", [name, amount])
+  redirect('/fruits')
+end
+
+get('/fruits') do
+  #Koppling till db
+  db = SQLite3::Database.new("db/fruits.db")
+  db.results_as_hash = true
+
+  query = params[:q]
+
+  if query && !query.empty?
+    @datafrukt = db.execute("SELECT * FROM fruits WHERE name LIKE ?","%#{query}%")
+  else
+    #hämta från db
+    @datafrukt = db.execute("SELECT * FROM fruits")
+  end
+  p @datafrukt
+
+  #Visa med slim
+  slim(:"fruits/index")
+end
+
+get('/new') do
+  slim(:"fruits/new")
+end
 
 get('/test') do
   @fruit = "Hugo Romeborn"
   slim(:test)
-  
-
 end
 
 get('/bye') do
@@ -15,7 +52,7 @@ get('/bye') do
 
 end
 
-get('/fruits') do
+get('/fruits1') do
   @fruits = ["banan", "apple", "pear"]
   slim(:fruits)
 
